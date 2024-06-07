c4ea45e3d7dfec0767fc21c4bd4e09d7ac23cd5c
Augusto Ribeiro <augustoribeiro100.g.j@gmail.com>
2024-06-07 11:46:16 -0300

    Criando backend para cadastro dos produtos

--- Application/controllers/Produto.php
+++ Application/controllers/Produto.php
@@ -8,4 +8,14 @@ class Produto extends Controller
     {
         $this->view('produto/cadastrar_produto');
     }
+
+    public function cadastrar_produto_sucesso()
+    {
+        $nome = $_POST['nome'];
+        $eco_valor = $_POST['eco_valor'];
+
+        $produtoModel = $this->model('Produtos');
+        $data = $produtoModel::cadastrar_produto($nome, $eco_valor);
+        $this->view('produto/cadastrar_produto_sucesso');
+    }
 }
---
+++ Application/models/Produtos.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Application\models;
+
+use Application\core\Database;
+use PDO;
+class Produto
+{
+    public static function cadastrar_produto($nome, $eco_valor)
+    {
+        $conn = new Database();
+        $result = $conn->executeQuery('INSERT INTO produto(nome, eco_valor) VALUES (:nome, :eco_valor)', array(
+            ':nome' => $nome,
+            ':eco_valor' => $eco_valor
+        ));
+
+        return True;
+    }
+
+    public static function cadastrar_operacao_entrada_produto($quantidade, $real_valor, $produto_id)
+    {
+        $conn = new Database();
+        $result = $conn->executeQuery('INSERT INTO produto_entrada(quantidade, real_valor, produto_id) VALUES(:quantidade, :real_valor, :produto_id)', array(
+            ':quantidade' => $quantidade,
+            ':real_valor' => $real_valor,
+            ':produto_id' => $produto_id
+        ));
+
+        return True;
+    }
+
+    public static function cadastrar_operacao_saida_produto($quantidade, $usuario_id, $produto_id, $eco_valor) {
+        $conn = new Database();
+        $result = $conn->executeQuery('INSERT INTO produto_saida(quantidade, usuario_id, produto_id, eco_valor) VALUES (:quantidade, :usuario_id, :produto_id, :eco_valor)', array(
+            ':quantidade' => $quantidade,
+            ':usuario_id' => $usuario_id,
+            ':produto_id' => $produto_id,
+            ':eco_valor' => $eco_valor
+        ));
+
+        return True;
+    }
+}
