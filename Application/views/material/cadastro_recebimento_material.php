ee8922f4eaff75045eb312ea5bba97aa071609a4
AfonsoFerroNunes <asonfo3@gmail.com>
2024-06-07 11:46:24 -0300

    criando as views do produto

--- Application/views/material/cadastro_recebimento_material.php
+++ Application/views/material/cadastro_recebimento_material.php
@@ -1,28 +1,28 @@
 <h1 class="display-4 text-center text-primary font-weight-bold mt-5 titulo">Recebimento de Material</h1>
 
-<form method="POST" action="../material/cadastro_recebimento_material_sucesso">
+<form method="POST" action="../material/cadastro_recebimento_material_sucesso" class="mt-5">
     
     <div class="mb-3">
-        <label class="font-weight-bold">Usuário</label>
-        <input type="text" name="usuario" class="form-control">
+        <label class="font-weight-bold" style="margin: 0% 22%">Usuário</label>
+        <input type="text" name="usuario" style="width:700px; margin: 0% 22%;" class="form-control">
     </div>
 
     <div class="mb-3">
-        <label class="font-weight-bold">Material</label>
-        <input type="text" name="material" class="form-control">
+        <label class="font-weight-bold" style="margin: 0% 22%">Material</label>
+        <input type="text" name="material" style="width:700px; margin: 0% 22%;" class="form-control">
     </div>
 
     <div class="mb-3">
-        <label class="font-weight-bold">Quantidade</label>
-        <input type="text" name="quantidade" class="form-control">
+        <label class="font-weight-bold" style="margin: 0% 22%">Quantidade</label>
+        <input type="text" name="quantidade" style="width:700px; margin: 0% 22%;" class="form-control">
     </div>
 
     <div class="mb-3">
-        <label class="font-weight-bold">Eco Points</label>
-        <input type="text" name="eco_valor" class="form-control">
+        <label class="font-weight-bold" style="margin: 0% 22%">Eco Points</label>
+        <input type="text" name="eco_valor" style="width:700px; margin: 0% 22%;" class="form-control">
     </div>
 
     <div class="mb-3">
-        <button type="submit" class= "btn btn-primary font-weight-bold" name="dados-categoria"  value="cadastrar-categoria">Enviar</button>
+        <button type="submit" class= "btn btn-primary font-weight-bold" name="dados-categoria" style="width:700px; margin: 0% 22%;" value="cadastrar-categoria">Enviar</button>
     </div>
 </form>
